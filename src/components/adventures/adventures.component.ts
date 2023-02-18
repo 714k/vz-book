7735f4c336fbbeee705228e3b7634412051ef03b
714k <victor.zamudio@hotmail.com>
2023-02-18 06:10:41 -0600

    [VZB-3] - Updating The Navigator texts

--- src/components/adventures/adventures.component.ts
+++ src/components/adventures/adventures.component.ts
@@ -2,14 +2,35 @@ import { Component, Input } from '@angular/core';
 
 import { Anchors } from '../header-navigation/header-navigation.component';
 
+type Link = {
+  title: string;
+  text: string;
+  href: string;
+}
+
+type Account = {
+  title: string;
+  text: string;
+  links?: Link[];
+};
+
+type Activity = {
+  title: string;
+  links?: Link[]
+}
+
+type Summary = {
+  activities: Activity[];
+  accounts: Account[];
+}
+
 type Adventure = {
   adventureNumber: number;
   period: string;
   company: string;
   industry: string;
   position: string;
-  activities: string;
-  accounts: string;
+  summary: Summary;
 }
 
 @Component({
