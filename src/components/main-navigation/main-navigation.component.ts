c410be4934e9fe97d2abaa0317c05287715730ea
714k <victor.zamudio@hotmail.com>
2023-03-05 03:36:31 -0600

    Updating Look & Feel The Navigator

--- src/components/main-navigation/main-navigation.component.ts
+++ src/components/main-navigation/main-navigation.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'vz-main-navigation',
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./main-navigation.component.scss']
 })
 export class MainNavigationComponent {
-  colorIcon: string = 'black';
+  @Input() colorIcon: string | undefined;
 }
